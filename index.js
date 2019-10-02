759481748c1b2c777f9e6ddadf67465a1e32075e
ekinalcar <ekinalcar@firmasoft.com>
2019-10-02 15:04:01 +0300

    finished auth flow

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+const authRoutes = require("./routes/authRoutes");
+require("./models/User");
+require("./services/passport");
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(
+  cookieSession({
+    //30 days
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    //cookie key
+    keys: [keys.cookieKey]
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("server started on port : " + PORT);
 });
