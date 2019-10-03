d074b1f2e210b45317c1ab1a119f17622034d18d
ekinalcar <ekinalcar@firmasoft.com>
2019-10-03 15:02:20 +0300

    added stripe client/frontend

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderContent() {
     switch (this.props.auth) {
       case null:
-        return "still deciding";
+        return;
       case false:
-        return "i am loggedout";
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
       default:
-        return "i am loged in";
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="2">
+            <a href="/api/logout">Logout</a>
+          </li>
+        ];
     }
   }
+
   render() {
+    const { auth } = this.props;
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link to={auth ? "/surveys" : "/"} className="left brand-logo">
+            Emaily
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 const authRoutes = require("./routes/authRoutes");
+const billingRoutes = require("./routes/billingRoutes");
+
 require("./models/User");
 require("./services/passport");
 
@@ -13,6 +16,9 @@ mongoose.connect(keys.mongoURI, {
 });
 
 const app = express();
+
+//middlewares
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     //30 days
@@ -25,7 +31,10 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+/*middleware end*/
+
 authRoutes(app);
+billingRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
